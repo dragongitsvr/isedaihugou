3e313775ea2e10a475714f16282290fe050f7a58
dragongitsvr <dragon1120git@gmail.com>
2022-11-24 10:35:24 +0900

    【タイトル】 不具合修正

--- Source/Assets/Photon/Controllers/TitleController.cs
+++ Source/Assets/Photon/Controllers/TitleController.cs
@@ -5,6 +5,7 @@ using System;
 using Photon.Services;
 using Unity.Collections;
 using Assets.Photon.Services;
+using Cysharp.Threading.Tasks;
 
 /// <summary>
 /// タイトル画面のコントローラー
@@ -59,7 +60,7 @@ public class TitleController : MonoBehaviour
     /// <summary>
     /// 「ログイン」ボタン押下時の処理
     /// </summary>
-    public void BtnLogin_Clicked()
+    public async void BtnLogin_Clicked()
     {
         // ローディング画面を表示
         var loadingService = gameObject.GetComponent<LoadingService>();
@@ -70,14 +71,9 @@ public class TitleController : MonoBehaviour
 
             // インスタンス※MonoBehaviourを継承している場合は、new禁止
             var titleService = gameObject.AddComponent<TitleService>();
-            var loginUser = titleService.LoginUser(_inpUserId.text);
 
-            // ログイン処理が完了するまでループ
-            while (loginUser.GetAwaiter().IsCompleted)
-            {
-                loadingService.CloseLoading();
-                return;
-            }
+            await titleService.LoginUser(_inpUserId.text);
+            loadingService.CloseLoading();
 
         }
         catch (Exception e)
